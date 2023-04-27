1f1e786add8e54a20bcc6fdc38db6289bf9cc95f
WuChengbo <79861030+WuChengbo-git@users.noreply.github.com>
2023-04-27 12:00:01 +0900

    feat: second aleph

---
+++ src/views/TimeLine.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <meeting-time-line />  <!-- 使用组件 -->
+  </div>
+</template>
+
+<script>
+  import MeetingTimeLine from '@/components/MeetingTimeLine'
+
+  export default {
+    components: {
+      MeetingTimeLine   // 注册组件
+    }
+  }
+</script>
